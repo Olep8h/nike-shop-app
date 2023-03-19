fe0db15290f48568ff4eca4f1eafac4d47f5c5ad
Olep8h <panov@student.vspj.cz>
2023-03-19 16:10:47 +0200

    + icon button, typography, icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "46aeb26a00a69730"; }
+/******/ 		__webpack_require__.h = function() { return "29f482716ff339c5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
